2a028d11f88a25cd9c40bb5c6e98db7e877da62a
sc21sv <sauliusvince@gmail.com>
2022-11-27 14:24:20 +0000

    Update tomeo.cpp
    Testing

--- 2811_cw2-master-release/tomeo.cpp
+++ 2811_cw2-master-release/tomeo.cpp
@@ -24,6 +24,7 @@
 #include <QtCore/QDirIterator>
 #include "the_player.h"
 #include "the_button.h"
+#include <QFrame>
 
 // read in videos and thumbnails to this directory
 std::vector<TheButtonInfo> getInfoIn (std::string loc)
@@ -86,8 +87,8 @@ int main(int argc, char *argv[])
     if (argc == 2)
         videos = getInfoIn( std::string(argv[1]) );
 
-    if (videos.size() == 0) {
-
+    if (videos.size() == 0)
+    {
         const int result = QMessageBox::information(
                     NULL,
                     QString("Tomeo"),
@@ -95,6 +96,7 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
+    /*
     // the widget that will show the video
     QVideoWidget *videoWidget = new QVideoWidget;
 
@@ -112,7 +114,8 @@ int main(int argc, char *argv[])
 
 
     // create the four buttons
-    for ( int i = 0; i < 4; i++ ) {
+    for ( int i = 0; i < 4; i++ )
+    {
         TheButton *button = new TheButton(buttonWidget);
         button->connect(button, SIGNAL(jumpTo(TheButtonInfo* )), player, SLOT (jumpTo(TheButtonInfo*))); // when clicked, tell the player to play.
         buttons.push_back(button);
@@ -121,21 +124,85 @@ int main(int argc, char *argv[])
     }
 
     // tell the player what buttons and videos are available
-    player->setContent(&buttons, & videos);
+    player->setContent(&buttons, &videos);
+    */
 
     // create the main window and layout
-    QWidget window;
-    QVBoxLayout *top = new QVBoxLayout();
-    window.setLayout(top);
-    window.setWindowTitle("tomeo");
-    window.setMinimumSize(800, 680);
+    QWidget *window = new QWidget();
+    //QVBoxLayout *top = new QVBoxLayout();
+    //window.setLayout(top);
+    window->setWindowTitle("tomeo");
+    //window->setMinimumSize(400, 720);
+
+    // the buttons are arranged horizontally
+//    QGridLayout* gridLayout = new QGridLayout(window);
+//    QHBoxLayout* header = new QHBoxLayout(window);
+//    QHBoxLayout* videoArea = new QHBoxLayout(window);
+//    QHBoxLayout* timeArea = new QHBoxLayout(window);
+//    QHBoxLayout* pauseArea = new QHBoxLayout(window);
+//    QHBoxLayout* videoEditingArea = new QHBoxLayout(window);
+//    QHBoxLayout* footer = new QHBoxLayout(window);
+
+//    gridLayout->addLayout(header, 0, 0);
+//    gridLayout->addLayout(videoArea, 1, 0);
+//    gridLayout->addLayout(timeArea, 2, 0);
+//    gridLayout->addLayout(pauseArea, 3, 0);
+//    gridLayout->addLayout(videoEditingArea, 4, 0);
+//    gridLayout->addLayout(footer, 5, 0);
+
+//    header->
+
+//    QFrame *label = new QFrame();
+//    QFrame *label2 = new QFrame();
+//    QFrame *label3 = new QFrame();
+//    QFrame *label4 = new QFrame();
+//    QFrame *label5 = new QFrame();
+//    QFrame *label6 = new QFrame();
+
+//    label->setLineWidth(2);
+//    label->setFrameStyle(QFrame::Panel);
+//    label2->setLineWidth(2);
+//    label2->setFrameStyle(QFrame::Panel);
+//    label3->setLineWidth(2);
+//    label3->setFrameStyle(QFrame::Panel);
+//    label4->setLineWidth(2);
+//    label4->setFrameStyle(QFrame::Panel);
+//    label5->setLineWidth(2);
+//    label5->setFrameStyle(QFrame::Panel);
+//    label6->setLineWidth(2);
+//    label6->setFrameStyle(QFrame::Panel);
+
+//    header->addWidget(label);
+//    videoArea->addWidget(label2);
+//    timeArea->addWidget(label3);
+//    pauseArea->addWidget(label4);
+//    videoEditingArea->addWidget(label5);
+//    footer->addWidget(label6);
+
+    QVBoxLayout* _mainLayout = new QVBoxLayout(window);
+    QHBoxLayout* header = new QHBoxLayout();
+    QHBoxLayout* videoArea = new QHBoxLayout(window);
+    QHBoxLayout* timeArea = new QHBoxLayout(window);
+
+    _mainLayout->addLayout(header);
+    _mainLayout->addLayout(videoArea);
+    _mainLayout->addLayout(timeArea);
+
+    QPushButton* button = new QPushButton("Header");
+    button->setGeometry(*new QRect(0, 0, window->width(), 10));
+    QPushButton* button1 = new QPushButton("Video Area");
+    QPushButton* button2 = new QPushButton("Time Area");
+
+    header->addWidget(button);
+    videoArea->addWidget(button1);
+    timeArea->addWidget(button2);
 
     // add the video and the buttons to the top level widget
-    top->addWidget(videoWidget);
-    top->addWidget(buttonWidget);
+    //top->addWidget(videoWidget);
+    //top->addWidget(buttonWidget);
 
     // showtime!
-    window.show();
+    window->show();
 
     // wait for the app to terminate
     return app.exec();
