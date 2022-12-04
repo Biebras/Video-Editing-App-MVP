07f91488fe11b23f4fee204642435058429248aa
sc21sv <sauliusvince@gmail.com>
2022-12-04 15:46:21 +0000

    More Classes

--- 2811_cw2-master-release/Video.h
+++ 2811_cw2-master-release/Video.h
@@ -1,11 +1,28 @@
 #ifndef VIDEO_H
 #define VIDEO_H
 
+#include <string>
+
+using namespace std;
 
 class Video
 {
-public:
-    Video();
+    private:
+        string _pathName;
+        int _start;
+        int _end;
+        int _volume;
+
+    public:
+        Video(string path, int start, int end, int volume);
+        string GetFilePath();
+        void SetStart(int start);
+        int GetStart();
+        void SetEnd(int end);
+        int GetEnd();
+        void SetVolume(int volume);
+        int GetVolume();
+        int GetDuration();
 };
 
 #endif // VIDEO_H
