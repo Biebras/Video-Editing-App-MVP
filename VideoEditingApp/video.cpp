f2806503d2d527468918937da06973da7b21e7f9
sc21sv <sauliusvince@gmail.com>
2022-12-06 10:43:42 +0000

    Update video.cpp

--- VideoEditingApp/video.cpp
+++ VideoEditingApp/video.cpp
@@ -2,10 +2,48 @@
 
 Video::Video(string path, int start, int end, int volume)
 {
-
+    _pathName = path;
+    _start = start;
+    _end = end;
+    _volume = volume;
 }
 
 string Video::GetFilePath()
 {
-    return NULL;
+    return _pathName;
+}
+
+void Video::SetStart(int start)
+{
+    _start = start;
+}
+
+int Video::GetStart()
+{
+    return _start;
+}
+
+void Video::SetEnd(int end)
+{
+    _end = end;
+}
+
+int Video::GetEnd()
+{
+    return _end;
+}
+
+void Video::SetVolume(int volume)
+{
+    _volume = volume;
+}
+
+int Video::GetVolume()
+{
+    return _volume;
+}
+
+int Video::GetDuration()
+{
+    return _end - _start;
 }
