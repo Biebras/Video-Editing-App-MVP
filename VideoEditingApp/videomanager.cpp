20ee2877e8fa479b2e4739c8f710c8dae92c2aae
sc21sv <sauliusvince@gmail.com>
2022-12-06 10:52:04 +0000

    Half video manager

--- VideoEditingApp/videomanager.cpp
+++ VideoEditingApp/videomanager.cpp
@@ -1,8 +1,22 @@
+#include <iostream>
 #include "videomanager.h"
 
 VideoManager::VideoManager()
 {
 
+}
+
+void VideoManager::AddVideo(Video* video)
+{
+    _videos.append(video);
+}
+
+void VideoManager::RemoveVideo(Video* video)
+{
+    bool success = _videos.removeOne(video);
+
+    if(!success)
+        std::cout << "Couldn't remove video" << std::endl;
 }
 
 Video* VideoManager::GetVideo(int milliseconds)
