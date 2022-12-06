2946269c2fd0271a0a3cf48588a27b3d10217fb5
sc21sv <sauliusvince@gmail.com>
2022-12-06 16:49:06 +0000

    Finnished

--- VideoEditingApp/videomanager.h
+++ VideoEditingApp/videomanager.h
@@ -14,6 +14,7 @@ class VideoManager
         void AddVideo(Video* video);
         void RemoveVideo(Video* video);
         Video* GetVideo(int milliseconds);
+        void PrintAllVideos();
 };
 
 #endif // VIDEOMANAGER_H
