f122033266d1b73c74f34fd11f37676777296af2
sc21sv <sauliusvince@gmail.com>
2022-12-07 12:39:53 +0000

    Singleton solution

--- VideoEditingApp/videomanager.h
+++ VideoEditingApp/videomanager.h
@@ -6,15 +6,24 @@
 
 class VideoManager
 {
-    private:
-        QList<Video*> _videos;
     public:
-        VideoManager(string conentPathName);
+        VideoManager(const VideoManager&) = delete;
+
+        static VideoManager& Get()
+        {
+            static VideoManager instance;
+            return instance;
+        };
+        void LoadVideos(string conentPathName);
         ~VideoManager();
         void AddVideo(Video* video);
         void RemoveVideo(Video* video);
         Video* GetVideo(int milliseconds);
         void PrintAllVideos();
+    private:
+        VideoManager(){}
+
+        QList<Video*> _videos;
 };
 
 #endif // VIDEOMANAGER_H
